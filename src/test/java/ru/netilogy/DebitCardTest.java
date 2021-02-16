92e05a78eb13f90649c475d22b3d566b0cea6d7b
AnastasiaKuznetzova <aleshka1923@gmail.com>
2021-02-16 14:12:49 +0300

    new test

--- src/test/java/ru/netilogy/DebitCardTest.java
+++ src/test/java/ru/netilogy/DebitCardTest.java
@@ -17,6 +17,15 @@ public class DebitCardTest {
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void shouldIncorrectFillingDebitCard(){
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("wert fddfdr");
+        $("[data-test-id=phone] input").setValue("+79658596936");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 
 }
 
